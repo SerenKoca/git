e5ada8951ac7f3b4b0718e9aed1d1d8ed5df2418
SerenKoca <r0937023@student.thomasmore.be>
2024-11-25 19:25:11 +0100

    databank

--- classes/Db.php
+++ classes/Db.php
@@ -1,12 +1,11 @@
 <?php
 namespace Web\XD;
 
-
-
 $pathToSSL = __DIR__ . '/CA.pem';
 $options = array(
     \PDO::MYSQL_ATTR_SSL_CA => $pathToSSL
 );
+
 
 
 
@@ -18,8 +17,11 @@ class Db {
         if (self::$conn == null) {
             try {
                 // Zorg ervoor dat je \PDO gebruikt, zodat het de globale PHP-klasse is
-
-                self::$conn = new \PDO('mysql:host=localhost;dbname=webshop', 'root', ''); 
+                $host = "webshopschool.mysql.database.azure.com";
+                $db = "webshopschool";
+                $user = "Seren";
+                $pass = "MLB11il!";
+                self::$conn = new \PDO("mysql:host=$host;dbname=$db", $user, $pass);  
                 self::$conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION); 
             } catch (\PDOException $e) {
                 throw new \Exception("Connection failed: " . $e->getMessage());
