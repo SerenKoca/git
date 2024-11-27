fad26054c1aff8bfe74c02c338e394ed9b10372f
SerenKoca <r0937023@student.thomasmore.be>
2024-11-27 09:16:50 +0100

    fix: path to classes

--- product_detail.php
+++ product_detail.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once(__DIR__ . "/classes/Db.php");
-include_once(__DIR__ . "/classes/Product.php");
+include_once(__DIR__ . '/classes/Db.php');
+include_once(__DIR__ . '/classes/Product.php');
 include_once(__DIR__ . '/classes/Comment.php');
 include_once(__DIR__ . '/classes/User.php');
 
